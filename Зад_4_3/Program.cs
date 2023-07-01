e00da812d099921a24b6df6693abeca7afdeb18c
S. Sergey <SS86RG@ya.ru>
2023-07-01 05:07:09 +0300

    fist commit

---
+++ Зад_4_3/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 3: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+//
+//(*) В 3-й задаче Доп сложность. Ввод с консоли “введите количество элементов массива”, 
+//“Введите минимальный порог случайных значений”, “Введите максимальный порог случайных значений”
+
+
+
+int[] GenerateArray(int len, int min, int max)
+{
+    int[] list1 = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < list1.Length; i++)
+    {
+        list1[i] = rnd.Next(min, max + 1);
+    }
+    return list1;
+}
+
+void PrintArrey(int[] Arrey)
+{
+    for (int i = 0; i < Arrey.Length; i++)
+    {
+        System.Console.Write(Arrey[i] + " ");
+    }
+}
+
+int[] number = GenerateArray(8,0,99);
+PrintArrey(number);
